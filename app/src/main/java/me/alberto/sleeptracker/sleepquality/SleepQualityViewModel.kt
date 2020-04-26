5fb03250c78f5d0258b646c6a58071c9f5b037dd
albertakpos <albertakpos@gmail.com>
2020-04-26 12:46:24 +0100

    added sleepViewModelFactory

--- app/src/main/java/me/alberto/sleeptracker/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/me/alberto/sleeptracker/sleepquality/SleepQualityViewModel.kt
@@ -1,2 +1,44 @@
 package me.alberto.sleeptracker.sleepquality
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import me.alberto.sleeptracker.database.SleepDatabaseDao
+
+class SleepQualityViewModel (
+    private val sleepNightKey: Long = 0L,
+    val database: SleepDatabaseDao): ViewModel(){
+
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+    val navigateToSleepTracker:LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onSetSleepQuality(sleepQuality: Int) {
+        uiScope.launch {
+            updateSleepNight(sleepQuality)
+            _navigateToSleepTracker.value = true
+        }
+    }
+
+    private suspend fun updateSleepNight(sleepQuality: Int) {
+        withContext(Dispatchers.IO){
+            val tonight = database.get(sleepNightKey) ?: return@withContext
+            tonight.sleepQuality = sleepQuality
+            database.update(tonight)
+        }
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
--- app/src/main/java/me/alberto/sleeptracker/sleepquality/SleepQualityViewModelFactory.kt
+++ app/src/main/java/me/alberto/sleeptracker/sleepquality/SleepQualityViewModelFactory.kt
@@ -1,2 +1,19 @@
 package me.alberto.sleeptracker.sleepquality
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import me.alberto.sleeptracker.database.SleepDatabaseDao
+import java.lang.IllegalArgumentException
+
+class SleepQualityViewModelFactory(
+    private val sleepNightKey: Long,
+    private val dataSource: SleepDatabaseDao
+): ViewModelProvider.Factory{
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepQualityViewModel::class.java)){
+            return SleepQualityViewModel(sleepNightKey, dataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
